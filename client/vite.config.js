2ff4e5e339a54bbf5788d18e5638b7dc7912faf6
Abdulrahman <abdulrahman939291@gmail.com>
2026-03-27 11:18:49 +0100

    Update the file vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,        // Allows Docker to access the dev server
+    port: 5173,        // Matches your Docker port
+    strictPort: true,  // Ensures it doesn't try to switch to 5174
+    watch: {
+      usePolling: true // Required for hot-reload to work inside Docker
+    },
+    // This is the "Magic" line that fixes the Black Page on refresh:
+    historyApiFallback: true, 
+  },
+  // Ensures all asset paths start with / (absolute paths)
+  base: '/', 
 })
